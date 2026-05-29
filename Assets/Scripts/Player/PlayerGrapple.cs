c4f2338dcea601cc8014a62fe859f523a61eaa74
Mennovos <mennodvos@gmail.com>
2026-05-29 14:45:02 +0200

    Feat:grapple progress

---
+++ Assets/Scripts/Player/PlayerGrapple.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerGrapple : MonoBehaviour
+{
+    LayerMask grappleLayerMask;
+    [SerializeField] private float GrappleSpeed = 5f;
+    [SerializeField] private Transform Grapplepoint;
+    [SerializeField] private List<Transform> grapplePoints;
+    [SerializeField] public List<GameObject> PickupsPosition;
+    private Controls Controls;
+    private bool grappling;
+    void Awake()
+    {
+        Controls = new Controls();
+        Controls.Player.Grapple.performed += Grapple;
+        grappleLayerMask = LayerMask.GetMask("Grappling");
+    }
+    IEnumerator GrappleCooldown()
+    {
+        //Anim.SetTrigger("Grapple");
+        yield return new WaitForSeconds(1f);
+        grappling = true;
+        yield return new WaitForSeconds(0.01f);
+        grappling = false;
+    }
+
+    public void Grapple(InputAction.CallbackContext context)
+    {
+
+        if (context.performed)
+        {
+            //StartCoroutine(GrappleCooldown());
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (Physics.Raycast(Grapplepoint.position, transform.forward, out RaycastHit hit, Mathf.Infinity, grappleLayerMask))
+        {
+            Debug.Log($"Grapple hit: {hit.collider.name}");
+            if (grappling == true && !hit.collider.CompareTag("Pickup"))
+            {
+                grapplePoints.Add(hit.transform);
+                Vector3 EndPoint = hit.point;
+            }
+            //if (grappling == true && hit.collider.CompareTag("Pickup"))
+            //{
+            //    PickupsPosition.Add(hit.collider.gameObject);
+            //}
+        }
+
+
+
+        for (int i = 0; i < grapplePoints.Count; i++)
+        {
+             transform.position = Vector3.Lerp(transform.position, grapplePoints[i].position, Time.deltaTime * GrappleSpeed);
+            if (Vector3.Distance(transform.position, grapplePoints[i].position) < 4f)
+            {
+                grapplePoints.RemoveAt(i);
+            }
+        }
+        for (int i = 0; i < PickupsPosition.Count; i++)
+        {
+            PickupsPosition[i].transform.position = Vector3.Lerp(PickupsPosition[i].transform.position, transform.position + new Vector3(0, 5, 0), Time.deltaTime * 2);
+        }
+    }
+}
