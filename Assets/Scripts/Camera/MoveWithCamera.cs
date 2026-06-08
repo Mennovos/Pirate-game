9ee852fdc34a5db859c245b0647a2ee2283544fe
ThunderstarVT <155100050+ThunderstarVT@users.noreply.github.com>
2026-06-08 11:31:29 +0200

    fix: move with camera uses center of screen instead of projected camera position

--- Assets/Scripts/Camera/MoveWithCamera.cs
+++ Assets/Scripts/Camera/MoveWithCamera.cs
@@ -6,6 +6,8 @@ public class MoveWithCamera : MonoBehaviour
     [SerializeField] private Transform cameraTransform;
     
     private float cameraDefaultDistance;
+    
+    private readonly Plane plane = new(Vector3.forward, Vector3.zero);
 
     private void Start()
     {
@@ -14,7 +16,14 @@ public class MoveWithCamera : MonoBehaviour
 
     private void Update()
     {
-        transform.position = new Vector3(cameraTransform.position.x, cameraTransform.position.y, 0);
-        transform.localScale = Vector3.one * cameraTransform.position.z / cameraDefaultDistance;
+        Ray ray = new Ray(cameraTransform.position, cameraTransform.forward);
+
+        if (plane.Raycast(ray, out float entryDistance))
+        {
+            Vector2 center = ray.GetPoint(entryDistance);
+
+            transform.position = center;
+            transform.localScale = Vector3.one * cameraTransform.position.z / cameraDefaultDistance;
+        }
     }
 }
