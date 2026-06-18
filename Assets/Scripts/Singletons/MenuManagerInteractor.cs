06612a8a9318ea9be15358a0a86b4e6c41b5ca99
ThunderstarVT <155100050+ThunderstarVT@users.noreply.github.com>
2026-06-18 09:55:12 +0200

    feat: select screen with preview

--- Assets/Scripts/Singletons/MenuManagerInteractor.cs
+++ Assets/Scripts/Singletons/MenuManagerInteractor.cs
@@ -1,8 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuManagerInteractor : MonoBehaviour
 {
     private MenuManager instance => MenuManager.Instance;
+
+
+    [SerializeField] private Image levelPreview;
+    [SerializeField] private Button nextLevelButton;
+    [SerializeField] private Button prevLevelButton;
+    
+    [SerializeField] private Sprite[] levelPreviewSprites;
+    
+    private int selectedLevelIndex;
+    
 
     public void OnReturnToMain()
     {
@@ -17,5 +28,35 @@ public class MenuManagerInteractor : MonoBehaviour
     public void OnQuitGame()
     {
         instance.OnQuitGame();
+    }
+
+
+    public void StartSelectedLevel()
+    {
+        instance.OnStartLevel(selectedLevelIndex);
+    }
+
+    public void SelectNextLevel()
+    {
+        selectedLevelIndex++;
+        // maybe clamp? should be impossible to overflow though (unless set up wrong)
+        
+        UpdateLevelPreview();
+    }
+
+    public void SelectPrevLevel()
+    {
+        selectedLevelIndex--;
+        // maybe clamp? should be impossible to overflow though (unless set up wrong)
+        
+        UpdateLevelPreview();
+    }
+
+    public void UpdateLevelPreview()
+    {
+        levelPreview.sprite = levelPreviewSprites[selectedLevelIndex];
+        
+        nextLevelButton.gameObject.SetActive(selectedLevelIndex + 1 < instance.LevelSceneNames.Length);
+        prevLevelButton.gameObject.SetActive(selectedLevelIndex > 0);
     }
 }
