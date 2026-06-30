ee7febd016d4a3a037061ce4f68feb24cd1803aa
Mennovos <mennodvos@gmail.com>
2026-06-30 12:39:03 +0200

    Feat:Better crab

--- Assets/Scripts/Enemy/Enemies/CrabEnemy3D.cs
+++ Assets/Scripts/Enemy/Enemies/CrabEnemy3D.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using UnityEditorInternal;
 using UnityEngine;
 
 public enum CrabState
@@ -7,32 +9,34 @@ public enum CrabState
     Death
 }
 
-[RequireComponent(typeof(Rigidbody), typeof(Collider), typeof(Animator))]
-public class CrabEnemy : MonoBehaviour
+public class CrabEnemy : Enemy
 {
     public float speed = 2f;
-    public float distance = 5f;
     public float chargeSpeed = 5f;
-    public float chargeCooldown = 3f;
     public float maxHealth = 30f;
+    public Vector3 pointA = new Vector3(-5f, 0f, 0f);
+    public Vector3 pointB = new Vector3(5f, 0f, 0f);
 
-    private Vector3 startPosition;
-    private int direction = 1;
     private CrabState currentState = CrabState.Walking;
     private float chargeTimer = 0f;
     private float currentHealth;
-    private Animator animator;
-    private bool hasReversed = false;
+    private Vector3 targetPoint;
+    private float lerpTime = 0f;
+    private float journeyLength;
+
+    [SerializeField] private float cooldown;
 
     void Start()
     {
-        startPosition = transform.position;
-        animator = GetComponent<Animator>();
         currentHealth = maxHealth;
+        transform.position = pointA;
+        targetPoint = pointB;
+        journeyLength = Vector3.Distance(pointA, pointB);
     }
 
     void Update()
-    {
+    { 
+        cooldown -= Time.deltaTime;
         if (currentState == CrabState.Death)
             return;
 
@@ -41,53 +45,42 @@ public class CrabEnemy : MonoBehaviour
         switch (currentState)
         {
             case CrabState.Walking:
-                HandleWalking();
-                if (chargeTimer >= chargeCooldown)
+                animator.SetBool("Walking", true);
+                lerpTime += Time.deltaTime * speed / journeyLength;
+
+                if (lerpTime >= 1f)
                 {
-                    ChangeState(CrabState.ChargeAttack);
+                    SwapTargetPoint();
+                    lerpTime = 0f;
                 }
+
+                Vector3 startPoint = targetPoint == pointB ? pointA : pointB;
+                transform.position = Vector3.Lerp(startPoint, targetPoint, lerpTime);
+
+              
                 break;
 
             case CrabState.ChargeAttack:
-                HandleChargeAttack();
+                animator.SetBool("Walking", false);
+                animator.ResetTrigger("Attacking");
+                animator.SetTrigger("Attacking");
+                StartCoroutine(WalkingAfterCharge());
                 break;
         }
     }
 
-    private void HandleWalking()
+    private void SwapTargetPoint()
     {
-        transform.position += new Vector3(direction * speed * Time.deltaTime, 0f, 0f);
-
-        float distanceFromStart = Mathf.Abs(transform.position.x - startPosition.x);
-
-        if (distanceFromStart >= distance && !hasReversed)
+        if (targetPoint == pointB)
         {
-            direction *= -1;
-            hasReversed = true;
+            targetPoint = pointA;
         }
-        else if (distanceFromStart < distance - 0.1f)
+        else
         {
-            hasReversed = false;
+            targetPoint = pointB;
         }
     }
 
-    private void HandleChargeAttack()
-    {
-        transform.position += new Vector3(direction * chargeSpeed * Time.deltaTime, 0f, 0f);
-
-        if (chargeTimer >= chargeCooldown + 1f)
-        {
-            ChangeState(CrabState.Walking);
-        }
-    }
-
-    private void ChangeState(CrabState newState)
-    {
-        currentState = newState;
-        chargeTimer = 0f;
-
-        animator.SetInteger("State", (int)currentState);
-    }
 
     public void TakeDamage(float damageAmount)
     {
@@ -104,7 +97,6 @@ public class CrabEnemy : MonoBehaviour
 
     public void Die()
     {
-        ChangeState(CrabState.Death);
         animator.SetTrigger("Die");
     }
 
@@ -121,5 +113,30 @@ public class CrabEnemy : MonoBehaviour
     public bool IsAlive()
     {
         return currentState != CrabState.Death;
+    }
+    private void OnTriggerEnter(Collider other)
+    { 
+        if (other.CompareTag("Player") && cooldown <= 0f)
+        {
+            currentState = CrabState.ChargeAttack;
+            cooldown = 2f;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+         if (collision.gameObject.CompareTag("Player"))
+        {
+            // Assuming the player has a Health component
+            Health playerHealth = collision.gameObject.GetComponent<Health>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(contactDamage);
+            }
+        }
+    }
+    private IEnumerator WalkingAfterCharge()
+    {
+        yield return new WaitForSeconds(2f);
+        currentState = CrabState.Walking;
     }
 }
